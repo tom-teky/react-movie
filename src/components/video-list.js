9d9321a2e348dc8497cbabb960dd4c62a86ce06f
Thomas Masson <t.massonn@gmail.com>
2021-01-31 04:22:44 +0100

    add axios queryy add props to videoÃ-list

--- src/components/video-list.js
+++ src/components/video-list.js
@@ -1,19 +1,17 @@
-import React from 'react'
-import VideoListItem from './video-list-item'
+import React from "react";
+import VideoListItem from "./video-list-item";
 
 const VideoList = () => {
-    const movies = ["film1", "film2", "film3"]
-    return (
-        <div>
-            <ul>
-                {
-                    movies.map((movie, index) => (
-                        <VideoListItem movie={movie} key={index} />
-                    ))
-                }
-            </ul>
-        </div>
-    )
-}
+  const movies = ["film1", "film2", "film3"];
+  return (
+    <div>
+      <ul>
+        {movies.map((movie, index) => (
+          <VideoListItem movie={movie} key={index} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default VideoList;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,38 @@
-import SearchBar from '../components/search-bar'
-import VideoList from '../components/video-list'
+import React, { Component } from "react"
+import SearchBar from "../components/search-bar"
+import VideoList from "../components/video-list"
+import VideoDetail from '../components/video-detail'
+import axios from "axios"
 
-const URL = "https://themoviedb.org/"
+const API_END_POINT = "https://api.themoviedb.org/3/"
 const API_KEY = "cb01c3a42a658cbeb1d04238ec2a5dee"
+const DEFAULT_TYPE_SEARCH ="discover"
+const DEFAULT_PARAM = "language=fr&include_adult=false"
 
-const App = () => {
-  return (
-    <div class="container">
-      <SearchBar />
-      <VideoList />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {movieList:{}, currentMovie:{}}
+  }
+
+  UNSAFE_componentWillMount() {
+    this.initMovies();
+  }
+
+  initMovies(){
+    axios.get(`${API_END_POINT}${DEFAULT_TYPE_SEARCH}/movie?api_key=${API_KEY}&sort_by=popularity.desc&${DEFAULT_PARAM}`).then(function(response){
+      this.setState({movieList:response.data.results.slice(1,6), currentMovie:response.data.results[0]})
+    }.bind(this))
+  }
+  render() {
+    return (
+      <div className="container">
+        <SearchBar />
+        <VideoList movieList={this.state.movieList} />
+        <VideoDetail title={this.state.currentMovie.title} description={this.state.currentMovie.overview}/>
+      </div>
+    )
+  }
 }
 
 export default App;

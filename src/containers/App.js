32d2e46c1faac9f764d8c7f2f658eeaa31d3265b
Thomas Masson <t.massonn@gmail.com>
2021-02-02 02:15:08 +0100

    add recommendation on search and click on list item

--- src/containers/App.js
+++ src/containers/App.js
@@ -59,6 +59,7 @@ class App extends Component {
   onClickListItem(movie) {
     this.setState({ currentMovie: movie }, function () {
       this.applyVideoToCurrentMovie();
+      this.setRecommendation();
     });
   }
 
@@ -71,11 +72,22 @@ class App extends Component {
             if (response.data.results[0].id !== this.state.currentMovie.id) {
               this.setState({ currentMovie: response.data.results[0] }, () => {
                 this.applyVideoToCurrentMovie();
+                this.setRecommendation();
               });
             }
           }
         }.bind(this));
     }
+  }
+
+  setRecommendation(){
+    axios
+      .get(`${API_END_POINT}movie/${this.state.currentMovie.id}/recommendations?${API_KEY}&language=fr`)
+      .then(
+        function (response) {
+          this.setState({movieList: response.data.results.slice(0, 5),});
+        }.bind(this)
+      );
   }
 
   render() {
